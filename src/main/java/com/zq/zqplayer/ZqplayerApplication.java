2078d8a20d37ca463e1c7bc7d4cac01d1991e153
wuyanqiang <wuyanqiang@inke.cn>
2020-09-10 17:03:06 +0800

    同时 支持 http、https

--- src/main/java/com/zq/zqplayer/ZqplayerApplication.java
+++ src/main/java/com/zq/zqplayer/ZqplayerApplication.java
@@ -1,9 +1,13 @@
 package com.zq.zqplayer;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan("com.zq.zqplayer.filter")//for filter指定扫描的包
@@ -12,6 +16,20 @@ public class ZqplayerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZqplayerApplication.class, args);
+    }
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(80);
+        return connector;
     }
 
 }
